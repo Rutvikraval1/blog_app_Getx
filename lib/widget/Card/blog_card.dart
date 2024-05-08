434f163c4ada549d894d6e3deb48772fddaf83ed
Rutvik raval <rutvikraval1@gmail.com>
2024-05-08 13:10:11 +0530

    chnage photo

--- lib/widget/Card/blog_card.dart
+++ lib/widget/Card/blog_card.dart
@@ -26,48 +26,62 @@ class BlogCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10.0),
-      child: InkWell(
-        onTap: () {
-          onTap();
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child:
-                Asset_imge_show().fadeInImage_network(image_path: imageUrl,boxFit: BoxFit.cover)
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        title,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: App_style().text_16_black_bold
-                      ),
-                      const SizedBox(height: 10.0),
-                      Text(
-                        'Author: $author',
-                        style: App_style().text_14
-                      ),
-                      Text(
-                        'Published at: ${Date_And_Time().getDateFormate(publishedAt)}',
-                        style:App_style().text_14
-                      ),
-                    ],
-                  ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child:
+              Asset_imge_show().fadeInImage_network(image_path: imageUrl,boxFit: BoxFit.cover)
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: App_style().text_16_black_bold
+                    ),
+                    const SizedBox(height: 10.0),
+                    Row(
+                      children: [
+                        Expanded(child:
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Author: $author',
+                                style: App_style().text_14
+                            ),
+                            Text(
+                                'Published at: ${Date_And_Time().getDateFormate(publishedAt)}',
+                                style:App_style().text_14
+                            ),
+                          ],
+                        )),
+                        TextButton(
+                          onPressed: () {
+                            onTap();
+                          },
+                          child: Text(
+                            'Read More',
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
